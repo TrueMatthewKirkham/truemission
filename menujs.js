b11c7075e5d349d81873f9a8bcfb2799736d9d09
Matthew Kirkham <matthewkirkham@Matthews-MacBook-Air.local>
2021-04-10 21:34:53 +0200

    hero fix and menu built

---
+++ menujs.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navlink');
+    const navlink = document.querySelectorAll('.navlink li')
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    
+
+        navlink.forEach((item, index) => {
+            if (item.style.animation) {
+                item.style.animation = '';
+            }
+            else {
+            console.log(item, index)
+            item.style.animation = `navlinkfade 0.1s ease forwards ${index /3 + 0.2}s`;
+            /* this is the order of 
+            arguments type, speed, way, 
+            something else, delay 
+            (delay based on the index used)' */
+        }});
+        burger.classList.toggle('toggle');
+    });
+    
+} 
+
+navSlide();
